de09589767703dde6c7bd900866bea1be923d56d
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-09-12 17:13:44 +0200

    fixed up some text made the loan system correctly with monthly payments

--- bank_account.rb
+++ bank_account.rb
@@ -1,22 +1,16 @@
 class Loan
   attr_accessor :amount, :monthly
-  def initialize(amount,loan)
+  def initialize(amount, loan)
     @id = loan.length + 1
     @amount = amount
-    @monthly = @amount * 0.1
+    @monthly = (@amount * 0.1).round(2)
   end
-  def self.monthly
-    @monthly
-  end
-  def self.amount
-    @amount
-  end
+
   def info
     puts "ID: #{@id}"
     puts "Loan Amount remaining: #{@amount}$"
     puts "Monthly payment: #{@monthly}$"
   end
-
 end
 
 puts "This is your new bank account. Enter first deposit:"
@@ -25,8 +19,6 @@ puts "Opened your account with balance: #{balance}$"
 puts "Personal interest is 3.7%"
 loan = []
 choice = true
-
-
 
 def deposit(balance)
   puts "--------------------\nCurrent Balance: #{balance}$\n--------------------"
@@ -39,9 +31,10 @@ end
 def withdraw(balance)
   puts "--------------------\nCurrent Balance: #{balance}$\n--------------------"
   puts "How much do you want to withdraw? "
-  new_balance = balance - gets.chomp.to_i
-  if 0 > new_balance
-    puts "You're trying to withdraw too much, insufficient fund"
+  amount = gets.chomp.to_i
+  new_balance = balance - amount
+  if new_balance < 0
+    puts "You're trying to withdraw too much, insufficient funds"
     balance
   else
     puts "--------------------\nNew Balance: #{new_balance}$\n--------------------"
@@ -53,43 +46,48 @@ def wait(balance, loan)
   interest = 3.7
   puts "How many days do you want to wait? "
   days = gets.chomp.to_i
-  new_balance =  balance + (days * ((balance * (interest / 100))/365)).round(2)
-  puts "--------------------\nCurrent Balance: #{balance}$\n--------------------"
+  interest_accrued = (days * ((balance * (interest / 100)) / 365)).round(2)
+  new_balance = balance + interest_accrued
+  puts "--------------------\nInterest accrued: #{interest_accrued}$\nNew Balance: #{new_balance}$\n--------------------"
+
   if loan.length > 0
-    loan.each { |i|
-      ((days / 30).floor).times do
-        i.amount = i.amount - i.monthly
-        if i.amount <= 0
+    loan.each do |i|
+      months = (days / 30).floor
+      months.times do
+        if i.amount <= i.monthly
+          new_balance -= i.amount
+          puts "Loan ID #{i.instance_variable_get(:@id)} fully paid off!"
           loan.delete(i)
+          break
+        else
+          new_balance -= i.monthly
+          i.amount -= i.monthly
         end
-        new_balance = new_balance - i.monthly
-        end
-    }
+      end
+    end
   end
   new_balance
 end
 
 def loans(loan)
-  puts "Interest is 10%\nWhat loan do you want to take out? "
+  puts "Interest is 10%\nWhat loan amount do you want to take out? "
   new_loan = Loan.new(gets.chomp.to_i, loan)
-  loan.append(new_loan)
+  loan << new_loan
 end
 
 def pay_loan(loan, id)
   puts id
-  puts loan[id-1]
+  puts loan[id - 1]
 end
 
 while choice
   if loan.length > 0
     puts "--------------------\n"
-    loan.each { |i|
-      i.info
-    }
+    loan.each(&:info)
     puts "--------------------"
   end
   puts "--------------------\nCurrent Balance: #{balance}$\n--------------------"
-  puts "Do you want to deposit, withdraw, wait, loan or delete account (1/2/3/4/5): "
+  puts "Do you want to deposit, withdraw, wait, loan, or delete account (1/2/3/4/5): "
   choice = gets.chomp.to_i
   case choice
   when 1
@@ -99,7 +97,7 @@ while choice
   when 3
     balance = wait(balance, loan)
   when 4
-    loan = loans(loan)
+    loans(loan)
   when 5
     choice = false
   else
